ceb3242883cd3fdd7bcb21ae1be3f85e43431d0f
Mator <why101@msn.com>
2017-09-24 14:36:42 -0700

    initial release

---
+++ index.js
@@ -0,0 +1,16 @@
+/* global ngapp, xelib, modulePath */
+//= require ./src/exampleService.js
+//= require ./src/exampleSettings.js
+
+ngapp.run(function(exampleService, settingsService) {
+    exampleService.helloWorld();
+
+    settingsService.registerSettings({
+        label: 'Example Module',
+        templateUrl: `${modulePath}/partials/exampleSettings.html`,
+        controller: 'exampleSettingsController',
+        defaultSettings: {
+            message: 'HI!'
+        }
+    });
+});
