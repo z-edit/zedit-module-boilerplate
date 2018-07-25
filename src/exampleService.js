8b3502066694e1dde165dc618c2ddf8b15689e16
Colin <why101@msn.com>
2018-07-24 21:33:32 -0700

    added example of using interApiService

--- src/exampleService.js
+++ src/exampleService.js
@@ -3,3 +3,11 @@ ngapp.service('exampleService', function() {
         console.log('Hello World!');
     };
 });
+
+// this code makes the exampleService accessible from
+// zEdit scripts and UPF patchers
+ngapp.run(function(exampleService, interApiService) {
+    interApiService.register({
+        api: { exampleService }
+    });
+});
